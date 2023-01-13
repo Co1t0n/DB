67a039356f3cd639304d7f01f0b7a56f39253077
Co1t0n <92403275+Co1t0n@users.noreply.github.com>
2023-01-12 19:27:13 -0500

    Create db.py

---
+++ db.py
@@ -0,0 +1,18 @@
+import time
+import webbrowser
+import ctypes  # An included library with Python install.
+def Mbox(title, text, style):
+    return ctypes.windll.user32.MessageBoxW(0, text, title, 0)
+Mbox('You have been shreked', 'Your time on earth is ogre', 1)
+
+timeout = time.time() + 3*5   
+while True:
+
+
+    test = 0
+webbrowser.open_new(https://media.tenor.com/lg0cvDLYL9EAAAAC/get-shrekt-shrek.gif)
+
+    if test == 5 or time.time() > timeout:
+        break
+    test = test - 1
+
